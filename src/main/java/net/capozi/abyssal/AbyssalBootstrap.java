43ebdc41a2044f7c3b0c4cda61a854cc1324edc3
capozi-devv <rslewis009@gmail.com>
2025-01-22 18:50:37 -0500

    Scraped the shadowfell dimesnsion, going to make custom loot to make the mirror dimension

--- src/main/java/net/capozi/abyssal/AbyssalBootstrap.java
+++ src/main/java/net/capozi/abyssal/AbyssalBootstrap.java
@@ -1,8 +1,6 @@
 package net.capozi.abyssal;
 
-import net.capozi.abyssal.block.AbyssalBlocks;
 import net.capozi.abyssal.items.AbyssalCombatItems;
-import net.capozi.abyssal.items.AbyssalFoods;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +12,5 @@ public class AbyssalBootstrap implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		AbyssalCombatItems.registerModItems();
-		AbyssalBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/capozi/abyssal/items/AbyssalFoods.java
+++
@@ -1,7 +0,0 @@
-package net.capozi.abyssal.items;
-
-
-
-public class AbyssalFoods {
-
-}
